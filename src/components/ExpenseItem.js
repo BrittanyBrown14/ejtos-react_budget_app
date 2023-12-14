31311fb76e6ea1fa729a232c1a3f9c1098e7cf7a
BrittanyBrown14 <kyana.brownstar@hotmail.co.uk>
2023-12-14 11:20:32 -0500

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch,currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -13,11 +13,24 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const increaseAllocation = (name) => {
+     const increaseAllocation = (name) => {
         const expense = {
             name: name,
             cost: 10,
-        };
+        }; 
+        
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+     const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };       
 
         dispatch({
             type: 'ADD_EXPENSE',
@@ -29,8 +42,37 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><button style={{
+                    backgroundColor: 'green',
+                    borderRadius: '50%',
+                    width: '35px',
+                    height: '35px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    border: 'none',
+                    paddingBottom: '10px',
+                    outline: 'none',
+                    cursor: 'pointer',
+                    color: 'white',
+                    fontSize: '2rem',
+                }}  onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{
+                    backgroundColor: 'red',
+                    borderRadius: '50%',
+                    width: '35px',
+                    height: '35px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    border: 'none',
+                    paddingBottom: '10px',
+                    outline: 'none',
+                    cursor: 'pointer',
+                    color: 'white',
+                    fontSize: '2rem',
+                }} onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
